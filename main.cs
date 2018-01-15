754c8e8e0c93b8c694e7e41e576e840dc97d7ee6
HybridRanger <34539950+HybridRanger@users.noreply.github.com>
2018-01-15 07:00:52 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -8,14 +8,35 @@ namespace Battleships
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int choice;
-
-            Boolean correct = false;
-
-            while (correct == false)
+            //dev
+            while (true)
             {
+                
+                int size = 10;
+                int[] ship_nums = new int[4];
+                ship_nums[0] = 1;
+                ship_nums[1] = 2;
+                ship_nums[2] = 1;
+                ship_nums[3] = 1;
+                string[,] display = new string[10, 10];
+                string[,] enemies = new string[10, 10];
+
+                Grid_Size(ref size, ref display, ref enemies);
+                Display(size, display);
+                //Select_Ships(size, ref ship_nums);
+                //Place_Enemies(size, enemies, ship_nums);
+
+                Console.ReadLine();
+            }
+            //dev
+            
+
+            while (true)
+            {
+                int choice;
                 Console.WriteLine("Main Menu:");
                 Console.WriteLine("1. Guessing Practice");
                 Console.WriteLine("2. Player vs AI");
@@ -30,22 +51,18 @@ namespace Battleships
                     case 1:
                         Console.Clear();
                         //goto guessing practice
-                        correct = true;
                         break;
                     case 2:
                         Console.Clear();
                         //goto player vs AI
-                        correct = true;
                         break;
                     case 3:
                         Console.Clear();
                         //goto 2 player
-                        correct = true;
                         break;
                     case 4:
                         Console.Clear();
                         //goto settings
-                        correct = true;
                         break;
                     case 5:
                         Console.Clear();
@@ -55,33 +72,7 @@ namespace Battleships
 
                 }
             }
-            while (true)
-            {
-                int size = 10;
-                int[] ship_nums = new int[4];
-                ship_nums[0] = 1;
-                ship_nums[1] = 2;
-                ship_nums[2] = 1;
-                ship_nums[3] = 1;
-                string[,] display = new string[10, 10];
-                string[,] enemies = new string[10, 10];
 
-                Grid_Size(ref size, ref display, ref enemies);
-                //Display(size, display);
-                //Select_Ships(size, ref ship_nums);
-                Place_Enemies(size, enemies, ship_nums);
-                for (int x = 0; x < size; x++)
-                {
-
-                    for (int y = 0; y < size; y++)
-                    {
-                        Console.Write(enemies[y, x]);
-                    }
-                    Console.WriteLine();
-                }
-
-                Console.ReadLine();
-            }
         }
 
         static void Grid_Size(ref int size, ref string[,] display, ref string[,] enemies)
@@ -102,9 +93,33 @@ namespace Battleships
         //set grid size
         static void Display(int size, string[,] display)
         {
+
+
+            Console.Write("  ");
+            for (int i = 0; i <=size-1; i++)
+            {
+                const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                var value = "";
+
+                if (i >= letters.Length)
+                    value += letters[i / letters.Length - 1];
+
+                value += letters[i % letters.Length];
+
+                    Console.Write(" " + value);
+
+            }
+            Console.WriteLine();
             for (int x = 0; x < size; x++)
             {
-
+                if ((x+1) < 10)
+                {
+                    Console.Write(" " + (x + 1));
+                } else
+                {
+                    Console.Write(x + 1);
+                }
                 for (int y = 0; y < size; y++)
                 {
                     Console.Write(display[y, x]);
@@ -113,6 +128,7 @@ namespace Battleships
             }
         }
         //display grid
+
         static void Place_Enemies(int size, string[,] enemies, int[] ship_nums)
         {
             for (int length = 2; length <= 5; length++)
@@ -167,7 +183,8 @@ namespace Battleships
                             if (temp[0, i] >= size || temp[0, i] <= 0 || temp[1, i] >= size || temp[1, i] <= 0)
                             {
                                 ship_placed = false;
-                            } else if (enemies[temp[1, i], temp[0, i]] == " #")
+                            }
+                            else if (enemies[temp[1, i], temp[0, i]] == " #")
                             {
                                 ship_placed = false;
                             }
@@ -180,17 +197,17 @@ namespace Battleships
                 }
             }
         }
-
         //place enemies
-        static void Enemy_Coords(ref int length, Boolean ship_placed, int[,] temp)
-        {
 
-        }
         static void Attack()
         {
+            Console.WriteLine("Enter coordinate (e.g. 3E)");
+            Console.WriteLine();
+            string coord = Console.ReadLine();
 
         }
         //attack
+
         static void Select_Ships(int size, ref int[] ship_nums)
         {
             Boolean exit = false;
